3c505259c1449661c9be62899a9b9e27dea8cb62
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-08-01 23:25:12 +0100

    demo
    demo

--- masos_setup.sh
+++ masos_setup.sh
@@ -2,7 +2,7 @@
 
 # This file is part of The MasOS Project
 #
-# The MasOS Project es legal, esta contruido bajo raspbian que es de codigo abierto, en este nuevo
+# The MasOS Project 2018 es legal, esta contruido bajo raspbian que es de codigo abierto, en este nuevo
 # sistema trabajan unos pocos desarroladores independientes de diversas partes del planeta.
 #
 # MasOS El sistema operativo exclusivo para la comunidad MyArcadeSpain ... de ahí su nombre.!
