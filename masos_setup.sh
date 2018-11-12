51585aaaf800dd29f9da9eb2d844baa9f704e88d
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-11-12 13:13:46 +0000

    Update masos_setup.sh

--- masos_setup.sh
+++ masos_setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# This file is part of The MasOS Project
+# This file is part of The MasOS Project fork de retropie
 #
 # The MasOS Project 2018 es legal, esta contruido bajo raspbian que es de codigo abierto, en este nuevo
 # sistema trabajan unos pocos desarroladores independientes de diversas partes del planeta.
 #
-# MasOS El sistema operativo para la comunidad MyArcadeSpain de ahí su nombre.! ,y para todo el que quiera...
+# MasOS sistema operativo fork de RetroPie ,proyecto iniciado en la comunidad MyArcadeSpain a mediados del año 2018 por dos de sus usuarios y desvinculada de la misma 5 meses despues por desiciones criticas de las 2 o 3 personas que gestionan MyArcadeSpain...
 #
 
 scriptdir="$(dirname "$0")"
