38d584c12b7619353bb6ff54ca625a3b37f74457
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2018-11-28 18:27:49 +0000

    Update systeminfo.sh
    masos info plus

--- scriptmodules/supplementary/retropiemenu/systeminfo.sh
+++ scriptmodules/supplementary/retropiemenu/systeminfo.sh
@@ -20,23 +20,27 @@ fi
 
 
 infobox= ""
-infobox="${infobox}\n$(date)\n"
-infobox="${infobox}\n$(uname -a)\n"
+infobox="${infobox}\nFecha: $(date)\n"
 infobox="${infobox}_______________________________________________________\n"
-infobox="${infobox}Temperatura\n\n"
+infobox="${infobox}\nVersion del Kernel: $(uname -a)\n"
+infobox="${infobox}_______________________________________________________\n\n"
+infobox="${infobox}Temperatura:\n\n"
 infobox="${infobox}CPU: ${cpuTempC} C/${cpuTempF} F\n"
 infobox="${infobox}GPU: ${gpuTempC} C/${gpuTempF} F\n"
 infobox="${infobox}\n"
 sdcard1=`df -h |head -2 |grep -v G|awk '{print $2, $3, $4}'`
 sdcard2=`df -h |head -2 |grep -v Size|awk '{print $2, $3, $4}'`
-infobox="${infobox}SD Card Informacion\n\n"
+infobox="${infobox}SD Card Informacion:\n\n"
 infobox="${infobox}${sdcard1}\n"
 infobox="${infobox}${sdcard2}\n"
 infobox="${infobox}\n"
+infobox="${infobox}_______________________________________________________\n"
+infobox="${infobox}\nUsuario: $(whoami)\n"
 infobox="${infobox}\n"
-infobox="${infobox}Su IP actual\n"
+infobox="${infobox}Nombre de host: $(hostname)\n\n"
+infobox="${infobox}Su IP actual:\n"
 infobox="${infobox}$(ip route get 8.8.8.8 2>/dev/null | awk '{print $NF; exit}')\n"
 
 dialog --backtitle "MasOS Informacion del Sistema" \
---title "Informacion del Sistema" \
---msgbox "${infobox}" 25 60
+--title "MasOS Info del Sistema" \
+--msgbox "${infobox}" 35 65
