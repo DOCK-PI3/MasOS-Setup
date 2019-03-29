9946715069d7e07c5bdccd13a9fae8f39020b71e
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-03-30 00:02:19 +0100

    Update Instalar musica de fondo.sh
    Añadidas opciones nuevas.. Activar o Desactivar la musica de fondo y limpiar directorio music de .mp3

--- scriptmodules/supplementary/retropiemenu/MasOS Utilidades/Instalar musica de fondo.sh
+++ scriptmodules/supplementary/retropiemenu/MasOS Utilidades/Instalar musica de fondo.sh
@@ -21,12 +21,14 @@ function main_menu() {
             --ok-label OK --cancel-label Exit \
             --menu "Que accion le gustaria realizar?" 25 75 20 \
             1 "Instalar MasOS Background Music" \
-			2 "Config Ruta Default MasOS BGM, opcional solo si falla la inicial " \
+			2 "Desactivar o Activar MasOS BGM ,solo despues de instalar" \
+            3 "Limpiar de .MP3 el directorio music" \
             2>&1 > /dev/tty)
 
         case "$choice" in
             1) Instalar_BGMUSICA  ;;
-			2) rutadefault_BGM  ;;
+			2) off_on_bgm  ;;
+			3) limpiar_mp3Dir  ;;
             *)  break ;;
         esac
     done
@@ -59,19 +61,40 @@ sudo chown -R pi:pi /home/pi/MasOS/roms/music
 sudo reboot
 }
 
-function rutadefault_BGM() {                                          #
-dialog --infobox "... MasOS Ruta por default musica de fondo para ES bgm ..." 30 55 ; sleep 3
-sudo chown -R pi:pi /etc/bgmconfig.ini
-   sudo cat > /etc/bgmconfig.ini <<_EOF_
-[default]
-startdelay = 0
-musicdir = /home/pi/MasOS/roms/music
-restart = True
-startsong =
-_EOF_
-echo -e "\n\n\n   El directorio por defecto donde introducir los .mp3 es \n/home/pi/MasOS/roms/music \n\n\n.Despues de meter sus mp3 reinicie el sistema. \n\n\n\n\n\nReiniciando en 7s"
-sleep 7
-sudo reboot
+#########################################################################
+# Activar o descativar la musica de fondo ,para es bgm #
+function off_on_bgm() {
+        if [[ -f /etc/bgmconfig.ini ]]; then
+			sudo chown -R pi:pi /etc/bgmconfig.ini
+			sudo mv /etc/bgmconfig.ini /etc/bgmconfig_off.ini
+			echo -e "\n\n\n   Desactivando la musica de fondo y reiniciando en 3s.\n\n\n"
+		sleep 3
+		sudo reboot
+    else
+        if [[ -f /etc/bgmconfig_off.ini ]]; then
+			sudo chown -R pi:pi /etc/bgmconfig_off.ini
+			sudo mv /etc/bgmconfig_off.ini /etc/bgmconfig.ini
+			echo -e "\n\n\n   Activando la musica de fondo y reiniciando en 3s.\n\n\n"
+		sleep 3
+		sudo reboot
+	fi
+		fi
+# ---------------------------- #
+}
+
+#########################################################################
+# Limpiar directorio de musica #
+function limpiar_mp3Dir() {
+        if [[ -f /home/pi/MasOS/roms/music/*.mp3 ]]; then
+			sudo chown -R pi:pi /home/pi/MasOS/roms/music
+			sudo rm -R /home/pi/MasOS/roms/music/*.mp3
+			echo -e "\n\n\n   Se eliminaron todos sus MP3.....\n\n\n"
+		sleep 3
+    else
+			echo -e "\n\n\n   El directorio de music esta vacio....\n\n\n"
+		sleep 2
+	fi
+# ---------------------------- #
 }
 
 main_menu
