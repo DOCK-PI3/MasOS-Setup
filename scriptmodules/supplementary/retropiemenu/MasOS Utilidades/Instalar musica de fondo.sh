b77ae4991b3ac1fd70cbaefa0ead23f8e4a19544
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-03-31 16:07:12 +0200

    Update Instalar musica de fondo.sh
    detector de cores emulators y ports...update :8ball:

--- scriptmodules/supplementary/retropiemenu/MasOS Utilidades/Instalar musica de fondo.sh
+++ scriptmodules/supplementary/retropiemenu/MasOS Utilidades/Instalar musica de fondo.sh
@@ -3,10 +3,11 @@
 infobox= ""
 infobox="${infobox}___________________________________________________________________________\n\n"
 infobox="${infobox}Instalador Herramienta Musica de fondo para ES , BGM MasOS\n\n"
-infobox="${infobox}30-03-2019...Version del script: 3.0a \n\n"
+infobox="${infobox}30-03-2019...Version del script: 3.0a \n"
+infobox="${infobox}Reproductor usado python-es-bgm ,creado por Rydra https://github.com/Rydra/bgm-for-es \n"
 infobox="${infobox}Install BGM MasOS Backgroud Music Tool for emulationstation\n\n"
 infobox="${infobox}-Scrip instalador creado por mabedeep para MasOS 4.X.X\n By MasOS Team\n\n"
-infobox="${infobox}NOTA: Agregadas las dos ultimas opciones para descargar \npaquete opcional de musica y el paquete por defecto. \n\n\n\n"
+infobox="${infobox}NOTA: Agregada opcion para actualizar el detector de cores BGM... \nMas de 70 emuladores y ports ,aparte los cores de retroarch añadidos!\n\n"
 infobox="${infobox}Script 100% funcional. Si falla alguna opcion avisenos por telegram! Salu2\n\n"
 infobox="${infobox}___________________________________________________________________________\n\n"
 
@@ -26,6 +27,7 @@ function main_menu() {
             3 "Limpiar de .MP3 el directorio music" \
 			4 "EXTRA BGM Descargar DOCK-PI3 ARCADE COLLECTIONS" \
 			5 "EXTRA BGM Descargar HEAZYHAX Default install" \
+			6 "BGM MasOS Detector de cores, emuladores y ports. ACTUALIZAR..." \
             2>&1 > /dev/tty)
 
         case "$choice" in
@@ -34,6 +36,7 @@ function main_menu() {
 			3) limpiar_mp3Dir  ;;
             4) dockpi3_bgmPack  ;;
 			5) default_bgmPack  ;;
+			6) detector_cores  ;;
 			*)  break ;;
         esac
     done
@@ -130,5 +133,199 @@ function default_bgmPack() {
 			sleep 3
 # ---------------------------- #
 }
+
+#########################################################################
+# Detector de cores ,emuladores y ports , bgm #
+function detector_cores() {
+			echo -e "\n\n\n   Reaparando/Actualizando el detector BGM MasOS.....Espere!.....\n\n\n"
+			sleep 3
+			sudo chown -R pi:pi /usr/lib/python2.7/dist-packages/bgm/Application.py
+		  sudo cp -R pi:pi /usr/lib/python2.7/dist-packages/bgm/Application.py /usr/lib/python2.7/dist-packages/bgm/Application_back.py
+		sudo cat > /usr/lib/python2.7/dist-packages/bgm/Application.py <<_EOF_
+import random
+import os
+from Queue import LifoQueue
+import time
+
+
+class State:
+    def __init__(self, name):
+        self.name = name
+
+State.paused = State("Paused")
+State.stopped = State("Stopped")
+State.playingMusic = State("PlayingMusic")
+
+
+class Application:
+    random.seed()
+
+    def getSongs(self):
+        return [song for song in os.listdir(self.musicdir) if song[-4:] == ".mp3" or song[-4:] == ".ogg"]
+
+    def getRandomQueue(self):
+
+        song_queue = LifoQueue()
+        song_list = self.getSongs()
+
+        if self.startsong in song_list:
+            song_queue.put(self.startsong)
+            song_list.remove(self.startsong)
+
+        while len(song_list) > 0:
+            song_to_add = random.randint(0, len(song_list) - 1)
+            song_queue.put(song_list[song_to_add])
+            song_list.remove(song_list[song_to_add])
+
+        return song_queue
+
+    # ACTIONS
+
+    def delay(self):
+        time.sleep(2)
+
+    def stopMusic(self):
+        self.musicPlayer.stop()
+
+    def fadeUp(self):
+        self.musicPlayer.fadeUpMusic()
+
+    def fadeDown(self):
+        self.musicPlayer.fadeDownMusic(not self.restart)
+
+    def playMusic(self):
+        if self.songQueue.empty():
+            self.songQueue = self.getRandomQueue()
+
+        song = os.path.join(self.musicdir, self.songQueue.get())
+        self.musicPlayer.playSong(song)
+
+    # CONDITIONS
+
+    def hasToStopMusic(self, status):
+        return not status["emulatorIsRunning"] and (status["musicIsDisabled"] or not status["esRunning"])
+
+    def emulatorIsNotRunning(self, status):
+        return not status["emulatorIsRunning"]
+
+    def shouldFadeDownAndStop(self, status):
+        return status["emulatorIsRunning"] and self.restart
+
+    def shouldFadeDownAndPause(self, status):
+        return status["emulatorIsRunning"] and not self.restart
+
+    def hasToPlayMusic(self, status):
+        return status["esRunning"] and not status["emulatorIsRunning"]
+
+    def musicIsNotPlaying(self, status):
+        return not self.musicPlayer.isPlaying
+
+    def __init__(self, process_service, music_player, settings, forced_initial_status = None):
+        self.processService = process_service
+        self.musicPlayer = music_player
+
+        self.startdelay = settings.getint("default", "startdelay")
+        self.musicdir = settings.get("default", "musicdir")
+        self.restart = settings.getboolean("default", "restart")
+        self.startsong = settings.get("default", "startsong")
+
+        self.songQueue = self.getRandomQueue()
+
+        self.emulatornames = ["retroarch", "ags", "advmame", "amiberry", "coolcv", "uae4all2", "uae4arm", "capricerpi", "linapple", "hatari", "stella",
+                              "atari800", "xroar", "vice", "dolphin", "daphne", "gearboy", "kat5200", "reicast", "pifba", "osmose", "gpsp", "jzintv",
+                              "basiliskll", "mame", "mame4all", "minivmac", "drastic", "dgen", "openmsx", "mupen64plus", "gngeo", "dosbox", "dosbox-sdl2",
+                              "ppsspp", "simcoupe", "np2", "pcsx2", "oricutron", "scummvm", "snes9x", "pisnes", "frotz", "fbzx", "fuse", "fs-uae", "gemrb",
+                              "cgenesis", "zdoom", "pcsx", "pokemini", "px68k", "quasi88", "eduke32", "lincity", "love", "kodi", "alephone", "micropolis",
+                              "openbor", "openttd", "rpix86", "sdltrs", "ti99sim", "uae4all", "opentyrian", "cannonball", "tyrquake", "ioquake3", "residualvm",
+                              "xrick", "sdlpop", "xm7", "zesarux", "uqm", "stratagus", "wolf4sdl", "ports", "chromium", "gamemaker", "retropie", "solarus"]
+
+        self.transitionTable = {
+            State.paused: [
+                (self.hasToStopMusic, self.stopMusic, State.stopped),
+                (self.emulatorIsNotRunning, self.fadeUp, State.playingMusic),
+                (None, self.delay, State.paused)],
+
+            State.stopped: [
+                (self.hasToPlayMusic, self.playMusic, State.playingMusic),
+                (None, self.delay, State.stopped)],
+
+            State.playingMusic: [
+                (self.hasToStopMusic, self.stopMusic, State.stopped),
+                (self.shouldFadeDownAndPause, self.fadeDown, State.paused),
+                (self.shouldFadeDownAndStop, self.fadeDown, State.stopped),
+                (self.musicIsNotPlaying, self.playMusic, State.playingMusic),
+                (None, self.delay, State.playingMusic)]
+        }
+
+        if forced_initial_status is None:
+            self.currentState = self.getInitialState()
+        else:
+            self.currentState = forced_initial_status
+
+    def getInitialState(self):
+        if self.musicPlayer.isPlaying:
+            return State.playingMusic
+        elif self.musicPlayer.isPaused:
+            return State.paused
+        else:
+            return State.stopped
+
+    def getState(self):
+
+        state = {
+            "musicIsDisabled": self.musicIsDisabled(),
+            "esRunning": self.processService.processIsRunning("emulationstatio"),
+            "emulatorIsRunning": self.processService.anyProcessIsRunning(self.emulatornames),
+            "songIsBeingPlayed": self.musicPlayer.isPlaying,
+            "restart": self.restart
+        }
+
+        return state
+
+    def waitForProcess(self, process_name):
+        while not self.processService.processIsRunning(process_name):
+            time.sleep(1)
+
+    def musicIsDisabled(self):
+        return os.path.exists('/home/pi/PyScripts/DisableMusic')
+
+    def waitomxPlayer(self):
+        # Look for OMXplayer - if it's running, someone's got a splash screen going!
+
+        while self.processService.processIsRunning("omxplayer"):
+            time.sleep(1)  # OMXPlayer is running, sleep 1 to prevent the need for a splash.
+
+        while self.processService.processIsRunning("omxplayer.bin"):
+            time.sleep(1)
+
+    def executeState(self):
+
+        status = self.getState()
+        transitions = self.transitionTable[self.currentState]
+        for condition, action, nextState in transitions:
+
+            if condition is None or condition(status):
+                action()
+                self.currentState = nextState
+                break
+
+    def run(self):
+
+        # Delay audio start per config option above
+        if self.startdelay > 0:
+            time.sleep(self.startdelay)
+
+        self.waitomxPlayer()
+
+        while True:
+            self.executeState()
+
+_EOF_
+				  # sudo chown -R pi:pi /usr/lib/python2.7/dist-packages/bgm
+				echo -e "\n\n\n   Detector actualizado correctamente!.....\n\n\nRecuerde reiniciar el sistema.!\n\n\n"
+			sleep 3
+# ---------------------------- #
+}
+
 
 main_menu
