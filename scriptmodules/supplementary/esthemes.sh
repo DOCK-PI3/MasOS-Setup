a42697af15abe52fb06f7561a68d37019dde2369
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-06-24 11:25:56 +0100

    Update esthemes.sh
    new ColdSummer es theme

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -48,6 +48,7 @@ function gui_esthemes() {
 		'DOCK-PI3 BASE-DEV'
 		'DOCK-PI3 BIGGRID'
 		'DOCK-PI3 bluepixel'
+		'DOCK-PI3 ColdSummer'
 		'DOCK-PI3 paralleluniverse'
 		'DOCK-PI3 Perinquen'
 		'DOCK-PI3 PlataFina'
